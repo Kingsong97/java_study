33e77c8c3311d540516917efea3f9e2b278acfea
kingsong <kingsongsm97@gmail.com>
2024-09-06 14:16:15 +0900

    docs: Scanner1 작성

--- src/loop/ex/NestedEx2.java
+++ src/loop/ex/NestedEx2.java
@@ -2,14 +2,17 @@ package loop.ex;
 
 public class NestedEx2 {
     public static void main(String[] args) {
-        int rows = 5;
 
-        for (int i = 1; i <= rows; i++){
+        int rows = 10;
+
+        for (int i = 0; i <= rows; i++){
             for (int k = 1; k <= rows - i; k++){
-                System.out.print(" ");
+                System.out.print(" ".repeat(i));
             }
-
-            for (int j = 1; j <= i; j++){
+            for (int j = 0; j <= i; j++){
+                System.out.print("*");
+            }
+            for (int j = 0; j < i; j++){
                 System.out.print("*");
             }
             System.out.println();
